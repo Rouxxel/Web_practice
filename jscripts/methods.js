4b03eba686ace73248a983501d1edf31bb571fe0
Rouxxel <106406623+Rouxxel@users.noreply.github.com>
2025-05-10 21:38:32 +0200

    Further project restructure and path modification

--- jscripts/methods.js
+++ jscripts/methods.js
@@ -33,7 +33,7 @@ function get_current_date(){
 }
 
 /*Image and subtext update functions*/
-const img_files=["imgs/playership.png","imgs/enemyship.png","imgs/enemyship2.png"];
+const img_files=["../imgs/playership.png","../imgs/enemyship.png","../imgs/enemyship2.png"];
 const img_subtexts=["Image by: John Doe", "Image by: Jane Doe", "Image by: Jan Doe"];
 let indx=0;
 
